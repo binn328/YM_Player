1e1182b8d5808cce8261c446e8d8667aac391ce9
binn328 <binn328@gmail.com>
2024-04-15 17:20:45 +0900

    프로젝트 초기 구성

---
+++ src/main/java/com/binn328/ym_player/YmPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.binn328.ym_player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmPlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binn328/ym_player/YmPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binn328.ym_player;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
