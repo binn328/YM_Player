b6c2749982b106b4f5393961343a1e98f60c4f61
YUJIN <127924591+22112030YUJIN@users.noreply.github.com>
2024-05-04 23:03:13 +0900

    커밋 중 뜸2

--- src/main/frontend/src/screens/musicupload/index.js
+++ src/main/frontend/src/screens/musicupload/index.js
@@ -29,7 +29,7 @@ function MusicForm() {
 
       console.log('Music uploaded');
 
-      // Reset form state after submission
+      // Reset
       setTitle('');
       setArtist('');
       setGroup('');
@@ -42,26 +42,30 @@ function MusicForm() {
   return (
     <div className="screen-container">
       <div className='container'>
-        <form onSubmit={handleSubmit} encType="multipart/form-data" className="music-form">
-          <div className="form-group">
-            <label>제목 </label>
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
-          </div>
-          <div className="form-group">
-            <label>가수 </label>
-            <input type="text" value={artist} onChange={(e) => setArtist(e.target.value)} required />
-          </div>
-          <div className="form-group">
-            <label>그룹 </label>
-            <input type="text" value={group} onChange={(e) => setGroup(e.target.value)} required />
-          </div>
-          <div className="form-group">
-            <label>파일 </label>
-            <input type="file" onChange={(e) => setFile(e.target.files[0])} required />
-          </div>
-          <input type="hidden" name="favorite" value="false" />
-          <button type="submit">확인</button>
-        </form>
+        <div className='form-box'>
+          <h1>Upload Music</h1>
+          <form onSubmit={handleSubmit} encType="multipart/form-data" className="music-form">
+            <div className="form-group">
+              <label>제목</label>
+              <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
+            </div>
+            <div className="form-group">
+              <label>가수</label>
+              <input type="text" value={artist} onChange={(e) => setArtist(e.target.value)} required />
+            </div>
+            <div className="form-group">
+              <label>그룹</label>
+              <input type="text" value={group} onChange={(e) => setGroup(e.target.value)} required />
+            </div>
+            <div className="form-group">
+              <label>파일</label>
+              <input type="file" onChange={(e) => setFile(e.target.files[0])} required />
+            </div>
+            <br></br>
+            <input type="hidden" name="favorite" value="false" />
+            <button type="submit">확인</button>
+          </form>
+        </div>
       </div>
     </div>
   );
