1975e100ba9151548259ef2e38f4fe3973243d3e
binn328 <binn328@gmail.com>
2024-05-06 23:41:47 +0900

    yt-dlp 다운로드 API 구현 시작

--- src/main/java/com/binn328/ym_player/Controller/DownloadAPIController.java
+++ src/main/java/com/binn328/ym_player/Controller/DownloadAPIController.java
@@ -1,12 +1,12 @@
 package com.binn328.ym_player.Controller;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Log4j2
-@RequestMapping("/")
-public class YtdlpAPIContorller {
+@RequestMapping("/api/dl")
+public class DownloadAPIController {
+
 }
--- src/main/java/com/binn328/ym_player/Service/DownloadService.java
+++ src/main/java/com/binn328/ym_player/Service/DownloadService.java
@@ -1,2 +1,16 @@
-package com.binn328.ym_player.Service;public class DownloadService {
+package com.binn328.ym_player.Service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * yt-dlp를 통한 다운로드를 담당하는 서비스
+ */
+@Service
+public class DownloadService {
+    private static final String ytdlpPath = "";
+
+    public String download(String url) {
+
+        return "";
+    }
 }
