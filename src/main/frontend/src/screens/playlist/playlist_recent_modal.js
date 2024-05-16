be19412e6456321fa49b2afb1c5ee5df91500943
YUJIN <127924591+22112030YUJIN@users.noreply.github.com>
2024-05-16 16:57:44 +0900

    플레이리스트 css수정

--- src/main/frontend/src/screens/playlist/playlist_recent_modal.js
+++ src/main/frontend/src/screens/playlist/playlist_recent_modal.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './recent.css'; // recent.css 파일 import
+/*import React, { useState, useEffect } from 'react';
+import './recent.css'; 
 
 const PlaylistRecentModal = ({ isOpen, toggleModal }) => {
   const [recentlyAdded, setRecentlyAdded] = useState([]);
@@ -40,6 +40,69 @@ const PlaylistRecentModal = ({ isOpen, toggleModal }) => {
           <button onClick={toggleModal}>닫기</button>
         </div>
 
+       
+        {selectedSong && (
+          <div className="song-info-modal">
+            <div className="modal">
+              <h2>{selectedSong.title}</h2>
+              <p>Artist: {selectedSong.artist}</p>
+              <p>Group: {selectedSong.group}</p>
+              <p>Favorite: {selectedSong.favorite ? 'Yes' : 'No'}</p>
+              <button onClick={closeSongInfo}>닫기</button>
+            </div>
+          </div>
+        )}
+      </div>
+    )
+  );
+};
+
+export default PlaylistRecentModal;
+*/
+
+
+import React, { useState, useEffect } from 'react';
+import './recent.css'; 
+
+const PlaylistRecentModal = ({ isOpen, toggleModal }) => {
+  const [recentlyAdded, setRecentlyAdded] = useState([]);
+  const [selectedSong, setSelectedSong] = useState(null); 
+
+  // 서버에서 최근에 추가된 곡 목록 가져오기
+  useEffect(() => {
+    fetch('http://localhost:8080/api/music')
+      .then(response => response.json())
+      .then(data => {
+     
+        setRecentlyAdded(data.reverse());
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  const handleSongClick = (song) => {
+    setSelectedSong(song);
+  };
+
+  const closeSongInfo = () => {
+    setSelectedSong(null);
+    toggleModal(); // 모달 닫기
+  };
+
+  return (
+    isOpen && (
+      <div className="recently-added-modal">
+        <div className="modal">
+          <h2>최근에 추가한 곡</h2>
+          <ul className="recently-added-list">
+            {recentlyAdded.map((song, index) => (
+              <li key={index} onClick={() => handleSongClick(song)}>
+                {song.title}
+              </li>
+            ))}
+          </ul>
+          <button onClick={toggleModal}>닫기</button>
+        </div>
+
         {/* 선택한 곡 정보 모달 */}
         {selectedSong && (
           <div className="song-info-modal">
@@ -58,3 +121,4 @@ const PlaylistRecentModal = ({ isOpen, toggleModal }) => {
 };
 
 export default PlaylistRecentModal;
+
