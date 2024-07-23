c29e1cd5bc09879fe5c592a1ac431afae792421c
binn328 <binn328@gmail.com>
2024-07-23 18:25:50 +0900

    init

---
+++ src/main/java/com/binn328/ymplayerremake/YMplayerRemakeApplication.java
@@ -0,0 +1,13 @@
+package com.binn328.ymplayerremake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YMplayerRemakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YMplayerRemakeApplication.class, args);
+    }
+
+}
