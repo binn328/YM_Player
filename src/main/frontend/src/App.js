112695db30a5549cdb944c6f35d18463c0dc0085
binn328 <binn328@gmail.com>
2024-07-23 18:29:04 +0900

    add react project

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/Header";
+import MyRoutes from "./routes/MyRoutes";
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <MyRoutes />
+        </div>
+    );
+}
+
+export default App;
