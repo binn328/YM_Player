c5226e2e6abea8d97e100ba003888b8c0a71f335
binn328 <binn328@gmail.com>
2024-07-23 18:54:59 +0900

    add frontend project in java project

---
+++ src/main/java/com/binn328/ymplayerremake/Controller/WebController.java
@@ -0,0 +1,16 @@
+package com.binn328.ymplayerremake.Controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 요청을 받으면 페이지를 반환해주는 컨트롤러
+ */
+@Controller
+public class WebController implements ErrorController {
+    @GetMapping({"/", "/error"})
+    public String homePage() {
+        return "index.html" ;
+    }
+}
